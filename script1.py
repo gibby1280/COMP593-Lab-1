5646e614abbd895ec42b4261522015d70e874961
gibby1280 <122818557+gibby1280@users.noreply.github.com>
2023-01-29 18:51:27 -0500

    Created script files
    Added code that prints lines from zen python

---
+++ script1.py
@@ -0,0 +1,3 @@
+script1.py
+print("Special cases aren't special enough to break the rules")
+print("Explicit is better than implicit.")
---
+++ script2.py
@@ -0,0 +1,3 @@
+script2.py
+print("omplex is better than complicated.")
+print("Namespaces are one honking great idea -- let's do more of those!")
