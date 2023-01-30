efe01e57f395f87a174a45cf32f7e722178da7aa
gibby1280 <122818557+gibby1280@users.noreply.github.com>
2023-01-29 19:04:30 -0500

    Changed both files
    removed print line from script1 and added it to script2

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Prints lines from zen python
 print("omplex is better than complicated.")
 print("Namespaces are one honking great idea -- let's do more of those!")
+print("Special cases aren't special enough to break the rules")
